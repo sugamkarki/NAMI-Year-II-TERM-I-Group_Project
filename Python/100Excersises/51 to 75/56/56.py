93695380b91c6996afdba8663965a063f2e63a40
magusikrak <sugamkarki7058@gmail.com>
2020-11-17 15:23:50 +0545

    sugam

---
+++ Python/100Excersises/51 to 75/56/56.py
@@ -0,0 +1,11 @@
+import json
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+# with open("company1.json", "w") as file:
+#     json.dump(d, file, indent=4)
+file=open("company.json",'w')
+json.dump(d,file,indent=4)
